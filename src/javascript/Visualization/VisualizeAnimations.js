20b461d3dc5a6bff1874420c6c0fae298e97041c
Adrian Forde <adrianforde02@gmail.com>
2021-03-06 01:40:58 -0400

    Added a slider that the user can use to change the animation speed of the algorithms

--- src/javascript/Visualization/VisualizeAnimations.js
+++ src/javascript/Visualization/VisualizeAnimations.js
@@ -1,11 +1,28 @@
 import { isRunning, setIsFinished, setIsRunning } from "../index.js";
 
-const visitedNodeAnimTime = 15;
-const backTrackNodeAnimTime = 40;
+const speedSlider = document.querySelector("#algo-speed-slider");
+const speedSliderValue = document.querySelector("#algo-speed-slider-value");
+
+let visitedNodeAnimTime = 100;
+let backTrackNodeAnimTime = 40;
+
+if (speedSlider !== null && speedSliderValue !== null) {
+  visitedNodeAnimTime = parseInt(speedSlider.value);
+  // backTrackNodeAnimTime = parseInt(speedSlider.value);
+  speedSliderValue.innerHTML = speedSlider.value;
+
+  speedSlider.addEventListener("change", (e) => {
+    visitedNodeAnimTime = parseInt(e.target.value);
+    // backTrackNodeAnimTime = parseInt(e.target.value);
+    speedSliderValue.innerHTML = e.target.value;
+  });
+}
 
 //Visualizes the algorithm
 export const visualizeAlgorithm = (visitedNodes, backTrackArray) => {
   setIsRunning(true);
+  speedSlider.disabled = true;
+  console.log(visitedNodes);
 
   if (
     document.querySelector("#visualize-btn") &&
@@ -67,6 +84,7 @@ export const visualizeAlgorithm = (visitedNodes, backTrackArray) => {
   setTimeout(() => {
     setIsRunning(false);
     setIsFinished(true);
+    speedSlider.disabled = false;
     if (
       document.querySelector("#visualize-btn") &&
       document.querySelector("#visualize-btn").classList.contains("disabled")
